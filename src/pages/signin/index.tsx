7db089ebbb6cef0e82a028ad367accbb3872c784
harshith kalki <harshithkalki@gmail.com>
2022-12-04 20:30:59 +0530

    added login and signup

---
+++ src/pages/signin/index.tsx
@@ -0,0 +1,77 @@
+import {
+  TextInput,
+  PasswordInput,
+  Checkbox,
+  Anchor,
+  Paper,
+  Title,
+  Text,
+  Container,
+  Group,
+  Button,
+  Divider,
+} from "@mantine/core";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import {
+  GoogleButton,
+  TwitterButton,
+} from "../../components/Icons/SocialButtons";
+
+export default function AuthenticationTitle() {
+  const [isLoadiing, setLoading] = useState(false);
+  const router = useRouter();
+  return (
+    <Container size={420} my={40}>
+      <Title
+        align="center"
+        sx={(theme) => ({
+          fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+          fontWeight: 500,
+        })}
+      >
+        Log in to your account
+      </Title>
+      <Text color="dimmed" size="sm" align="center" mt={5}>
+        Do not have an account yet?{" "}
+        <Anchor<"a">
+          size="sm"
+          onClick={(event) => {
+            event.preventDefault();
+            router.push("/signup");
+          }}
+        >
+          Create account
+        </Anchor>
+      </Text>
+
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <TextInput label="Email" placeholder="you@email.com" required />
+        <PasswordInput
+          label="Password"
+          placeholder="Your password"
+          required
+          mt="md"
+        />
+        <Group position="apart" mt="lg">
+          <Checkbox label="Remember me" sx={{ lineHeight: 1 }} />
+          <Anchor<"a">
+            onClick={(event) => event.preventDefault()}
+            href="#"
+            size="sm"
+          >
+            Forgot password?
+          </Anchor>
+        </Group>
+        <Button fullWidth mt="xl">
+          Sign in
+        </Button>
+        <Divider label="Or continue with " labelPosition="center" my="lg" />
+        <Group grow mb="md" mt="md">
+          <GoogleButton radius="xl">Google</GoogleButton>
+          <TwitterButton radius="xl">Twitter</TwitterButton>
+        </Group>
+      </Paper>
+    </Container>
+  );
+}
